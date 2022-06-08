fe2bc684536bf1b5cd37a9f7d968d6f31b329edb
Pedrazaa11 <pedrazaa1122@gmail.com>
2022-06-08 19:10:39 +0200

    Creacion de pantallas

---
+++ app/src/main/java/com/dam/zoomcars/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.dam.zoomcars;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
