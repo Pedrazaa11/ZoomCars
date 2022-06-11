6d9896daf6cad6b3043a6b180e2398140aeef6d6
IgnacioBarbeyto <84562597+IgnacioBar@users.noreply.github.com>
2022-06-11 19:42:45 +0200

    Crear MiPerfilActivity.java y hacer que el boton de Mi Perfil abra el activity MiPerfilActivity.java y crear iconos de info(tutorial) y buscar.

--- app/src/main/java/com/dam/zoomcars/MainActivity.java
+++ app/src/main/java/com/dam/zoomcars/MainActivity.java
@@ -2,7 +2,10 @@ package com.dam.zoomcars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +16,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void IrMiPerfil(View v) {
+        Intent i = new Intent(this, MiPerfilActivity.class);
+        startActivity(i);
+    }
+
     public void salirApp(View v) {
         finish();
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu mimenu) {
+
+        getMenuInflater().inflate(R.menu.menumain, mimenu);
+
+        return true;
+
+    }
+
+    public boolean oOptionItemSelected(MenuItem opcion_menu) {
+
+        int id= opcion_menu.getItemId();
+
+        if (id==R.id.MisVH) {
+            return true;
+        }
+
+        if (id==R.id.MisChats) {
+            return true;
+        }
+
+        if (id==R.id.MiPerfil) {
+            IrMiPerfil(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(opcion_menu);
     }
 }
