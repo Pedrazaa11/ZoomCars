c481599ce5b7bcbc730588fd7b2ac54c3b261289
Pedrazaa11 <pedrazaa1122@gmail.com>
2022-06-13 18:27:30 +0200

    AñadirVhFragment codigo de java

--- app/src/main/java/com/dam/zoomcars/fragments/AñadirVhFragment.java
+++ app/src/main/java/com/dam/zoomcars/fragments/AñadirVhFragment.java
@@ -1,66 +1,195 @@
 package com.dam.zoomcars.fragments;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.dam.zoomcars.MainActivity;
 import com.dam.zoomcars.R;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AñadirVhFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.HashMap;
+
+
 public class AñadirVhFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Uri imageUri;
+    String myUrl = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AñadirVhFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AñadirVhFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AñadirVhFragment newInstance(String param1, String param2) {
-        AñadirVhFragment fragment = new AñadirVhFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    EditText etMarca;
+    EditText etModelo;
+    EditText etAñoFab;
+    EditText etCV;
+    EditText etPuertas;
+    EditText etCombustible;
+    EditText etCambio;
+    EditText etKm;
+    EditText etDescripcion;
+    ImageView ivFotoCoche;
+    ImageView ibAdjuntarFoto;
+    Button btnPostCoche;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aniadir_vh, container, false);
+        View view = inflater.inflate(R.layout.fragment_aniadir_vh, container, false);
+
+        etMarca = view.findViewById(R.id.etMarcaVH);
+        etModelo = view.findViewById(R.id.etModeloVH);
+        etAñoFab = view.findViewById(R.id.etAñoVH);
+        etCV = view.findViewById(R.id.etCvVH);
+        etPuertas = view.findViewById(R.id.etPuertasVH);
+        etCombustible = view.findViewById(R.id.etCombustibleVH);
+        etCambio = view.findViewById(R.id.etCambioVH);
+        etKm = view.findViewById(R.id.etKmVH);
+        etDescripcion = view.findViewById(R.id.etDescripcionVH);
+
+        ivFotoCoche = view.findViewById(R.id.ivFotoCoche);
+        ibAdjuntarFoto = view.findViewById(R.id.ibAdjuntarFoto);
+
+        btnPostCoche = view.findViewById(R.id.btnPostCoche);
+
+        storageReference = FirebaseStorage.getInstance().getReference("Coches");
+
+        btnPostCoche.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etMarca.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir una marca de coche", Toast.LENGTH_SHORT).show();
+                }else if (etModelo.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir un modelo de coche", Toast.LENGTH_SHORT).show();
+                }else if (etAñoFab.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir un año de fabricacion del coche", Toast.LENGTH_SHORT).show();
+                }else if (etCV.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir los caballos del coche", Toast.LENGTH_SHORT).show();
+                }else if (etPuertas.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir el Nº de puertas del coche", Toast.LENGTH_SHORT).show();
+                }else if (etCombustible.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir un tipo de combustible", Toast.LENGTH_SHORT).show();
+                }else if (etCambio.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir un tipo de cambio de marchas", Toast.LENGTH_SHORT).show();
+                }else if (etKm.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir el kilometraje del coche", Toast.LENGTH_SHORT).show();
+                }else if (etDescripcion.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Debes escribir una descripcion del vehiculo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        ibAdjuntarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                startActivityForResult(Intent.createChooser(intent, "Complete la acción usando"), 1);
+            }
+        });
+
+
+        return view;
     }
+
+
+    //TODO: metodo para pasar la imagen a Ploopfragment
+   /* @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            // Cargamos la imagen seleccionada en el ImageView
+            imageUri = data.getData();
+            Glide.with(ivFotoEvento.getContext()).load(imageUri)
+                    .into(ivFotoEvento);
+        }
+
+    } */
+
+    private void uploadImage() {
+        ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Publicando Coche");
+        progressDialog.show();
+
+        if (imageUri != null) {
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis() + "");
+            uploadTask = fileReference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        myUrl = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Loops");
+
+                        String postId = reference.push().getKey();
+
+                        HashMap<String, Object> hashMap = new HashMap<>();
+                        hashMap.put("id", postId);
+                        hashMap.put("marca", etMarca.getText().toString());
+                        hashMap.put("modelo", etModelo.getText().toString());
+                        hashMap.put("añodefabricacion", etAñoFab.getText().toString());
+                        hashMap.put("cv", etCV.getText().toString());
+                        hashMap.put("puertas", etPuertas.getText().toString());
+                        hashMap.put("combustible", etCombustible.getText().toString());
+                        hashMap.put("cambio", etCambio.getText().toString());
+                        hashMap.put("kilometraje", etKm.getText().toString());
+                        hashMap.put("descripcion", etDescripcion.getText().toString());
+                        hashMap.put("imageUrl", myUrl);
+                        hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(postId).setValue(hashMap);
+                        progressDialog.dismiss();
+                        startActivity(new Intent(getActivity(), MainActivity.class));
+
+                    } else {
+                        Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "No has seleccionado ninguna imagen", Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+        }
+    }
+
+
+
 }
