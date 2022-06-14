6d25c48282856408a687792d5f4879811f42193d
Pedrazaa11 <pedrazaa1122@gmail.com>
2022-06-14 21:24:16 +0200

    Creacion de mi perfil y EditarPerfilActivity

--- app/src/main/java/com/dam/zoomcars/MiPerfilActivity.java
+++ app/src/main/java/com/dam/zoomcars/MiPerfilActivity.java
@@ -2,13 +2,45 @@ package com.dam.zoomcars;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MiPerfilActivity extends AppCompatActivity {
+import com.dam.zoomcars.model.Usuario;
+
+public class MiPerfilActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvUsername;
+    TextView tvFullname;
+    Button btnEditperfil;
+    Usuario user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mi_perfil);
+
+        tvUsername = findViewById(R.id.tvUsuarioM);
+        tvFullname = findViewById(R.id.tvNombreM);
+        btnEditperfil = findViewById(R.id.btnEditperfilM);
+
+        btnEditperfil.setOnClickListener(this);
+
+        tvFullname.setText(user.getFullname());
+        tvUsername.setText(user.getUsername());
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.equals(btnEditperfil)){
+            Intent i = new Intent(MiPerfilActivity.this, EditarPerfilActivity.class);
+            startActivity(i);
+        }
+
     }
 }
