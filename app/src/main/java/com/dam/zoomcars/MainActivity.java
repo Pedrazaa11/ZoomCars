7ceb86ba3456ba712d5d5eabfe6e2b0e2975e829
IgnacioBarbeyto <84562597+IgnacioBar@users.noreply.github.com>
2022-06-14 20:20:29 +0200

    Menu, MisChatsActivity.java y MisVhActivity.java

--- app/src/main/java/com/dam/zoomcars/MainActivity.java
+++ app/src/main/java/com/dam/zoomcars/MainActivity.java
@@ -1,41 +1,25 @@
 package com.dam.zoomcars;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    public void IrMiPerfil(View v) {
-        Intent i = new Intent(this, MiPerfilActivity.class);
-        startActivity(i);
-    }
-
-    public void IrTutorial(View v) {
-        Intent i = new Intent(this, TutorialActivity.class);
-        startActivity(i);
-    }
-
-    public void IrInfo(View v) {
-        Intent i = new Intent(this, InfoActivity.class);
-        startActivity(i);
-    }
-
-    public void cerrarSesion(View v) {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
     }
 
     @Override public boolean onCreateOptionsMenu(Menu mimenu) {
@@ -46,37 +30,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public boolean oOptionItemSelected(MenuItem opcion_menu) {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_misvh:
+                Intent i = new Intent(MainActivity.this, MisVhActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_mischats:
+                Intent i2 = new Intent(MainActivity.this, MisChatsActivity.class);
+                startActivity(i2);
+                break;
+            case R.id.nav_miperfil:
+                Intent i3 = new Intent(this, MiPerfilActivity.class);
+                startActivity(i3);
+                break;
+            case R.id.nav_tutorial:
+                Intent i4 = new Intent(this, TutorialActivity.class);
+                startActivity(i4);
+                break;
+            case R.id.nav_sobrenosotros:
+                Intent i5 = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(i5);
+                break;
+            case R.id.nav_cerrarsesion:
+                MaterialAlertDialogBuilder alertDialog = new MaterialAlertDialogBuilder(MainActivity.this);
+                alertDialog.setMessage("¿Estás seguro que deseas salir?");
+                alertDialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //firebaseAuth.signOut();
+                        finish();
+                    }
+                });
+                alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-        int id= opcion_menu.getItemId();
-
-        if (id==R.id.nav_misvh) {
-            return true;
+                    }
+                });
+                alertDialog.create().show();
+                break;
         }
 
-        if (id==R.id.nav_mischats) {
-            return true;
-        }
-
-        if (id==R.id.nav_miperfil) {
-            IrMiPerfil(null);
-            return true;
-        }
-
-        if (id==R.id.nav_tutorial) {
-            IrTutorial(null);
-            return true;
-        }
-
-        if (id==R.id.nav_sobrenosotros) {
-            IrInfo(null);
-            return true;
-        }
-
-        if (id==R.id.nav_cerrarsesion) {
-            cerrarSesion(null);
-        }
-
-        return super.onOptionsItemSelected(opcion_menu);
+        return true;
     }
 }
