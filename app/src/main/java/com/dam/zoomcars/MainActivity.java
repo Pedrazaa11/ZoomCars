218248d0ebdf763afd0d795817e9e1d7e01262b5
IgnacioBarbeyto <84562597+IgnacioBar@users.noreply.github.com>
2022-06-15 09:04:21 +0200

    Menu nuevo

--- app/src/main/java/com/dam/zoomcars/MainActivity.java
+++ app/src/main/java/com/dam/zoomcars/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -24,41 +25,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     FirebaseAuth firebaseAuth;
 
-    private DrawerLayout drawer;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
-        setSupportActionBar(toolbar);
 
-        /*getSupportFragmentManager().beginTransaction().replace(R.id.nav).commit();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -67,7 +55,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menumain, menu);
+        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
         return true;
     }
 
@@ -75,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_misvh:
-                Intent i = new Intent(MainActivity.this, MisVhActivity.class);
+                Intent i = new Intent(MainActivity.this, activity.class);
                 startActivity(i);
                 break;
             case R.id.nav_mischats:
@@ -116,6 +104,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         DrawerLayout drawer=(DrawerLayout)findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+*/
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int title;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_misvh:
+                title = R.string.;
+                break;
+            case R.id.nav_gallery:
+                title = R.string.menu_gallery;
+                break;
+            case R.id.nav_manage:
+                title = R.string.menu_tools;
+                break;
+            case R.id.nav_share:
+                title = R.string.menu_share;
+                break;
+            case R.id.nav_send:
+                title = R.string.menu_send;
+                break;
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        Fragment fragment = HomeContentFragment.newInstance(getString(title));
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.nav_enter, R.anim.nav_exit)
+                .replace(R.id.home_content, fragment)
+                .commit();
+
+
+        setTitle(getString(title));
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
         return true;
     }
 
